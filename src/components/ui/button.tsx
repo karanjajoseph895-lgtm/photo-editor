2ab0ba3b549ce395bb97aed10f1e25a21dcc3f2e
karanjajoseph895-lgtm <karanjajoseph895@gmail.com>
2026-06-09 17:47:11 +0300

    Fix ButtonProps interface declaration

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -30,13 +30,12 @@ const buttonVariants = cva(
 )
 
 export interface ButtonProps
- export interface ButtonProps
-  extends React.ButtonHTMLAttributes<<HTMLButtonElement>,
-    VariantProps<<typeof buttonVariants> {
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
   asChild?: boolean
 }
 
-const Button = React.forwardRef<<HTMLButtonElement, ButtonProps>(
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
     return (
